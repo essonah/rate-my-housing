0bf9754e4d5a64ab11563e292e80af587c2a5c0d
AkosuaNyarks <aknyarkomah@gmail.com>
2024-07-30 08:11:53 -0400

    Add login page and sign Up page

--- rate-my-housing/src/components/Login.jsx
+++ rate-my-housing/src/components/Login.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React,{useState} from "react";
+
+export const Login=({onFormSwitch})=>{
+    const[email,setEmail]=useState('');
+    const[pass,setPass]=useState('');
+    
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        console.log(email);
+
+    }
+
+    return(
+        <div className="auth-form-container">
+            <h2>Login</h2>
+        <form  className="login-form" onSubmit={handleSubmit}>
+           
+
+            <label htmlFor="email">Email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
 
 
+            <label htmlFor="password">Password</label>
+            <input value={pass} onChange={(e) =>setPass(e.target.value)}type="password" placeholder="*********" id="password" name="password"/>
+
+            <button className="login_btn" type='submit'>Login</button>
+        </form>
+        <button  className="link-btn" onClick={() =>onFormSwitch('signin')}>Don't have an account.Signup here</button>
+        </div>
+    );
+
+
+}
 
 // function Login(){
 //     return(
