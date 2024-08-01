31bb2ef5d90672b23349f184f29840d44c490911
essonah <amandaadjei128@gmail.com>
2024-07-31 21:51:24 -0400

    updated

---
+++ client/src/App 2.js
@@ -0,0 +1,29 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Rate from './components/Rate';
+import Navbar from './components/navbar';
+import DormModal from './components/DormModal';
+import ReviewsPage from './components/ReviewsPage';
+import FAQ from './components/faq';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rate" element={<Rate />} />
+            <Route path="/navbar" element={<Navbar />} />
+            <Route path="/dorm/:id" element={<DormModal />} />
+           <Route path="/reviews" element={<ReviewsPage />} />
+            <Route path="/faq" element={<FAQ />} />
+          </Routes>
+        </BrowserRouter>
+      </header>
+    </div>
+  );
+}
+
+export default App;
