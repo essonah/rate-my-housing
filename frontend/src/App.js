6bb8a7eebdfc4504fa3d11db8439cab7fb63e520
essonah <amandaadjei128@gmail.com>
2024-11-23 12:23:58 -0500

    clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,12 @@ import Home from './components/Home';
 import About from './components/About';
 import FAQ from './components/faq';
 import Testimonials from './components/Testimonials';
+import ReviewsPage from './components/ReviewsPage';
+import Navbar from './components/navbar';
+import Rate from './components/Rate';
+import DormModal from './components/DormModal';
+import ReviewDorm from './components/ReviewDorm';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -12,9 +18,15 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/rate" element={<Rate />} />
+        <Route path ="/navbar" element={<Navbar />} />
+        <Route path= "/dorm:id" element={<DormModal />} />
         <Route path="/about" element={<About />} />
         <Route path="/faq" element={<FAQ />} />
         <Route path="/testimonials" element={<Testimonials />} />
+        <Route path ="/reviews" element={<ReviewsPage />} />
+        <Route path ="reviewdorm" element={<ReviewDorm />} />
+        <Route path = "/login" element={<Login />} />
        
       </Routes>
       </BrowserRouter>
