0145b2b828cb44fdc212357a1223e316f6c3dddd
essonah <amandaadjei128@gmail.com>
2024-11-23 12:14:19 -0500

    removed extra files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
-import Rate from './components/Rate';
-import Navbar from './components/navbar';
-import DormModal from './components/DormModal';
-import ReviewsPage from './components/ReviewsPage';
+import About from './components/About';
 import FAQ from './components/faq';
-import ReviewDorm from './components/ReviewDorm';
-import Login from './components/Login';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <BrowserRouter basename="/rate-my-housing">
-          <Routes>
-            <Route path="/" element={<div>Home Page</div>} />
-            <Route path="/rate" element={<Rate />} />
-            <Route path="/navbar" element={<Navbar />} />
-            <Route path="/dorm/:id" element={<DormModal />} />
-            <Route path="/reviews" element={<ReviewsPage />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/reviewdorm" element={<ReviewDorm />} />
-            <Route path="/login" element={<Login />} />
-            {/* Catch-all route */}
-            <Route path="*" element={<div>404 - Page Not Found</div>} />
-          </Routes>
-        </BrowserRouter>
-      </header>
+    <div className='App'>
+      <header className='header'></header>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+       
+      </Routes>
+      </BrowserRouter>
+     
     </div>
   );
 }
-
 export default App;
